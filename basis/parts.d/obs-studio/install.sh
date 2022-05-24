4a0f0fbdba80b65f6de2d11a9a11145ace06c0ce
Basil Stotz <stotz.basil@opinsys-q0AXi4JA>
2022-05-24 17:55:31 +0200

    remove self compiled obs-studio

--- basis/parts.d/obs-studio/install.sh
+++ basis/parts.d/obs-studio/install.sh
@@ -9,9 +9,10 @@ echo *****************************obs***************************************
 # install and unpack
 
 #latest obs-studio (self compiled)
-if ! test -f obs-studio_20210107-git-1_amd64.deb;then
-    wget https://www.amxa.ch/debian/packages/obs-studio_20210107-git-1_amd64.deb
-fi
+#if ! test -f obs-studio_20210107-git-1_amd64.deb;then
+#    wget https://www.amxa.ch/debian/packages/obs-studio_20210107-git-1_amd64.deb
+#fi
+
 #plugin websocket
 if ! test -e obs-websocket-4.8.0-1_amd64.deb;then
   wget https://github.com/Palakis/obs-websocket/releases/download/4.8.0/obs-websocket-4.8.0-1_amd64.deb
@@ -23,7 +24,7 @@ fi
 
 
 #install
-apt-get -y remove obs-studio obs-plugins libobs0
+#apt-get -y remove obs-studio obs-plugins libobs0
 test -d /usr/share/obs/obs-plugins/obs-rtspserver/ || mkdir -p /usr/share/obs/obs-plugins/obs-rtspserver/
 tar -xzvf obs-rtspserver-v1.2.1-linux.tar.gz -C /usr/share/obs/obs-plugins/obs-rtspserver/
 for D in *.deb; do
