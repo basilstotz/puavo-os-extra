116832b58a34374f734a066e24a0f7975c19d7b5
Basil Stotz <stotz.basil@opinsys-q0AXi4JA>
2022-05-27 07:37:56 +0200

    geary-backports is back

---
+++ basis/parts.d/geary-backports/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd $(dirname "$0")
+
+DIST=$(cat /etc/issue.net |cut -d\  -f3)
+
+case  $DIST in
+    10)
+       apt-get -y -t buster-backports install geary
+       ;;
+    11)
+       apt-get -y install geary
+       ;;
+    *)
+       ;;
+esac
+        
+
+echo "*****************************geary***************************************"
