285bbfb1fd5af2c10b9bae02c0e4652b695bf226
Basil Stotz <stotz.basil@opinsys.fi>
2022-07-06 13:55:55 +0200

    fix install.sh

--- patch/final/parts.d/printer-ppds/install.sh
+++ patch/final/parts.d/printer-ppds/install.sh
@@ -3,28 +3,13 @@
 cd $(dirname $0)
 
 #downlaod
-
-if ! test -d brands;then
-   wget https://amxa.ch/debian/tars/printer-brands.tar.gz
-   tar -xzf printer-brands.tar.gz
-fi
-
 echo -n "printer-ppds installing ....."
 
-
-
-for P in brands/*; do
-    $P/install.sh
-done
-
-
-#patch for canon
-if test -d /opt/cel/; then
-    ln -s /opt/cel/ppd/ /usr/share/cups/model/
-    mv /usr/share/cups/model/ppd /usr/share/cups/model/cel
-    ln -s /opt/cel/bin/sicgsfilter /usr/lib/cups/filter/
-    ln -s /opt/cel/bin/sicnc /usr/lib/cups/filter/      
+if ! test -f cups-printer-ppd.tgz; then
+    wget https://amxa.ch/debian/tars/cups-printer-ppd.tgz
 fi
+
+tar -xzf cups-printer-ppd.tgz -C /
 
 
 echo "done"
